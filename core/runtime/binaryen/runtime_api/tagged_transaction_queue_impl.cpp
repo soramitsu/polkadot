e107b7955477903de8b3a432eb0c5214acd28d22
Alexander Krutikov <krutikov@soramitsu.co.jp>
2021-07-19 15:40:53 +0300

    fix transaction validity check

--- core/runtime/binaryen/runtime_api/tagged_transaction_queue_impl.cpp
+++ core/runtime/binaryen/runtime_api/tagged_transaction_queue_impl.cpp
@@ -11,14 +11,24 @@ namespace kagome::runtime::binaryen {
   TaggedTransactionQueueImpl::TaggedTransactionQueueImpl(
       const std::shared_ptr<RuntimeEnvironmentFactory> &runtime_env_factory)
       : RuntimeApi(runtime_env_factory),
-        logger_{log::createLogger("TaggedTransactionQueue", "binarien")} {}
+        logger_{log::createLogger("TaggedTransactionQueue", "binarien")} {
+  }
+
+  void TaggedTransactionQueueImpl::setBlockTree(
+      std::shared_ptr<blockchain::BlockTree> block_tree) {
+    block_tree_ = std::move(block_tree);
+  }
 
   outcome::result<primitives::TransactionValidity>
   TaggedTransactionQueueImpl::validate_transaction(
       primitives::TransactionSource source, const primitives::Extrinsic &ext) {
+    BOOST_ASSERT(block_tree_);
+    auto hash = block_tree_->deepestLeaf().hash;
+    OUTCOME_TRY(best_header, block_tree_->getBlockHeader(hash));
     logger_->trace("{}", ext.data.toHex());
-    return execute<TransactionValidity>(
+    return executeAt<TransactionValidity>(
         "TaggedTransactionQueue_validate_transaction",
+        best_header.state_root,
         CallConfig{.persistency = CallPersistency::EPHEMERAL},
         source,
         ext,
