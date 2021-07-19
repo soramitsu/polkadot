e7f5b56900bf7a2fdb41644c0b41472c34afc95e
Harrm <abrehchs@gmail.com>
2021-07-19 11:50:31 +0300

    Pick correct state for validate_transaction runtime call

--- core/runtime/binaryen/runtime_api/tagged_transaction_queue_impl.cpp
+++ core/runtime/binaryen/runtime_api/tagged_transaction_queue_impl.cpp
@@ -9,14 +9,20 @@ namespace kagome::runtime::binaryen {
   using primitives::TransactionValidity;
 
   TaggedTransactionQueueImpl::TaggedTransactionQueueImpl(
-      const std::shared_ptr<RuntimeEnvironmentFactory> &runtime_env_factory)
-      : RuntimeApi(runtime_env_factory) {}
+      const std::shared_ptr<RuntimeEnvironmentFactory> &runtime_env_factory,
+      std::shared_ptr<const blockchain::BlockTree> block_tree)
+      : RuntimeApi(runtime_env_factory), block_tree_{block_tree} {
+    BOOST_ASSERT(block_tree_);
+  }
 
   outcome::result<primitives::TransactionValidity>
   TaggedTransactionQueueImpl::validate_transaction(
       primitives::TransactionSource source, const primitives::Extrinsic &ext) {
-    return execute<TransactionValidity>(
+    OUTCOME_TRY(best_header,
+                block_tree_->getBlockHeader(block_tree_->deepestLeaf().hash));
+    return executeAt<TransactionValidity>(
         "TaggedTransactionQueue_validate_transaction",
+        best_header.state_root,
         CallConfig{.persistency = CallPersistency::EPHEMERAL},
         source,
         ext);
--- core/runtime/binaryen/runtime_api/tagged_transaction_queue_impl.hpp
+++ core/runtime/binaryen/runtime_api/tagged_transaction_queue_impl.hpp
@@ -9,19 +9,26 @@
 #include "runtime/binaryen/runtime_api/runtime_api.hpp"
 #include "runtime/tagged_transaction_queue.hpp"
 
+#include "blockchain/block_header_repository.hpp"
+#include "blockchain/block_tree.hpp"
+
 namespace kagome::runtime::binaryen {
 
   class TaggedTransactionQueueImpl : public RuntimeApi,
                                      public TaggedTransactionQueue {
    public:
     TaggedTransactionQueueImpl(
-        const std::shared_ptr<RuntimeEnvironmentFactory> &runtime_env_factory);
+        const std::shared_ptr<RuntimeEnvironmentFactory> &runtime_env_factory,
+        std::shared_ptr<const blockchain::BlockTree> block_tree);
 
     ~TaggedTransactionQueueImpl() override = default;
 
     outcome::result<primitives::TransactionValidity> validate_transaction(
         primitives::TransactionSource source,
         const primitives::Extrinsic &ext) override;
+
+   private:
+    std::shared_ptr<const blockchain::BlockTree> block_tree_;
   };
 }  // namespace kagome::runtime::binaryen
 
